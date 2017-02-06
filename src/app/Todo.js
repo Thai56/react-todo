f8231b72b18673c86c611b3c505dde53f3d9b2ea
Thai Tran <Thai56@Thais-MacBook-Air.local>
2017-02-06 03:03:23 -0700

    added adding feature

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
-
 export class Todo extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todos:[],
+      newTodo:''
+    }
+  }
+
+  handleClick(e){
+    e.preventDefault()
+    const todos = [...this.state.todos,this.input.value]
+    this.input.value='';
+    this.setState({todos})
+  }
+  handleChange(e){
+    const {value} = e.target;
+    this.setState({newTodo:value})
+  }
+  remove(i){
+    const todos = [...this.state.todos.slice(0,i),...this.state.todos.slice(i+1)]
+    this.setState({todos})
+  }
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form>
+          <input ref={node => this.input = node}/>
+          <button onClick={this.handleClick.bind(this)}>click</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo,i)=>{
+             return (
+               <li
+                 onClick={()=>{this.remove.call(this,i)}}
+                 key={i}>{todo}</li>
+             )
+           })}
+        </ul>
+      </div>
     );
   }
 }
